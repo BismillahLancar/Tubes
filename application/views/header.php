def115dcba9f76eb5371ecc992956b6e6fdeda5d
Rifky Syahbana <rifkysyahbana123@gmail.com>
2018-05-30 21:44:50 +0700

    admin kosong

--- application/views/header.php
+++ application/views/header.php
@@ -6,7 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="description" content="">
 	<meta name="author" content="FaberNainggolan">
-	<title>CodeIgniter dengan Bootstrap</title>
+	<title>Pemesanan Tiket Pesawat</title>
 
 
 	<link href="<?=base_url()?>assets/css/bootstrap.min.css"  rel="stylesheet">
@@ -19,6 +19,7 @@
 <nav class="navbar navbar-default navbar-static-top">
 <div class="container">
 <div class="navbar-header">
+	<h1 style="font-family:stencil;"><b><i>EASY.COM<i><b></h1>
 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-control="navbar">
 <span class="sr-only">Toggle navigation</span>
 </button>
@@ -26,7 +27,7 @@
 </div>
 <div id="navbar" class="navbar-collapse collapse">
 	<ul class="nav navbar-nav navbar-right">
-    <li><a href="<?=base_url()?>/index.php/Login"><i class="glyphicon glyphicon-user"></i>Login / Sign Up</a></li>
+    <li><a href="<?=base_url()?>/index.php/Admin"><i class="glyphicon glyphicon-user"></i>Login / Sign Up</a></li>
   </ul>
 
 	
